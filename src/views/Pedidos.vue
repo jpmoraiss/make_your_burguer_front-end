ac659bfea919e3c8b91970f6e2661b629844e21b
jpmoraiss <jpmorais.ca11@gmail.com>
2024-01-14 21:52:14 -0300

    initial commit

---
+++ src/views/Pedidos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Gerenciar pedidos:</h1>
+      <Dashboard />
+    </div>
+  </div>
+</template>
+
+<script>
+import Dashboard from '../components/Dashboard'
+
+export default {
+  name: 'Pedidos',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style scoped>
+
+.main-container {
+  min-height: calc(100vh - 200px);
+
+}
+
+</style>
